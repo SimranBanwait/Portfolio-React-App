fb0671960ddead00557992992ac617fdb800f156
Simran Banwait <simranbanwait02@gmail.com>
2023-05-05 12:52:16 +0530

    Added new files

---
+++ scripts/afterinsatll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd Workspace/Pipeline
+pm2 server build 8080 --spa
---
+++ scripts/beforeinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
+sudo apt install npm
+npm install pm2 -g
+npm install serve -g
