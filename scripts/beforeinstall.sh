b8ec9c26215184e74f3e834bd09cbd21ca3b0dac
Simran Banwait <simranbanwait02@gmail.com>
2023-05-08 12:56:22 +0530

    Chalega

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -4,10 +4,10 @@ cd home/ubuntu/Pipeline
 sudo apt-get update
 sudo apt-get upgrade
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install --lts
-sudo apt install nodejs
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install --lts
+# sudo apt install nodejs
 
 npm install
 # npm start
