29f84bcf4976d32049cb297c77641c6c52e48c4f
Simran Banwait <simranbanwait02@gmail.com>
2023-05-08 09:30:02 +0530

    Will work

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -7,9 +7,9 @@ sudo apt-get upgrade
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install --lts
-
+sudo apt install nodejs
 
 npm install
-npm start
+# npm start
 npm install pm2 -g
 npm install --global serve
