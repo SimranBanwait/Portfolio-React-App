7ee36b02d6c7ea37bd9ce43e6311eab2e86585a9
Simran Banwait <simranbanwait02@gmail.com>
2023-05-08 13:53:01 +0530

    ewjudg

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#changing directory
-cd home/ubuntu/Pipeline
 
 #updating build modules
 # sudo rm -rf node_modules
@@ -12,4 +10,7 @@ cd home/ubuntu/Pipeline
 # npm install
 npm install pm2 -g
 npm install --global serve
+
+#changing directory
+cd home/ubuntu/Pipeline
 pm2 server build 8080 --spa
